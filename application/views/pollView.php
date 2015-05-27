3010191219449615a8ccdef7bceebcf1ff21eca4
Jay Harris <jay.harris@outlook.co.nz>
2015-05-28 09:03:40 +1200

    Adds an 'Admin' button

--- application/views/pollView.php
+++ application/views/pollView.php
@@ -43,6 +43,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li><a href="#/about">About</a></li>
+                    <button type="button" class="btn btn-default navbar-btn">I'm an Admin!</button>
                 </ul>
             </div>
         </nav>

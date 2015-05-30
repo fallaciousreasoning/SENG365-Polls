289c9b3ab420afdf08f8a7dbaaa489b2c176710f
Jay Harris <jay.harris@outlook.co.nz>
2015-05-30 19:23:48 +1200

    Fixes some validation errors

--- application/views/pollView.php
+++ application/views/pollView.php
@@ -33,7 +33,7 @@
         <nav class="navbar navbar-default">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span
+                    <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -42,13 +42,13 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a class="navbar-brand" href="#/admin"><span class="glyphicon glyphicon-pencil nav-header">Admin</a></li>
-                    <li><a class="navbar-brand" href="#/about"><span class="glyphicon glyphicon-info-sign nav-header">About</a></li>
+                    <li><a class="navbar-brand" href="#/admin"><span class="glyphicon glyphicon-pencil nav-header">Admin</span></a></li>
+                    <li><a class="navbar-brand" href="#/about"><span class="glyphicon glyphicon-info-sign nav-header">About</span></a></li>
                 </ul>
             </div>
         </nav>
 
-		<div ng-view class="page"></div>
+		<div ng-view="" class="page"></div>
 
 	</body>
 </html>
